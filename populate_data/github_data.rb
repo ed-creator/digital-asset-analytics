bdf09887a9f9d01685aa739e1bae025f5cf3f39e
Edward Ward <edwardwardward@gmail.com>
2017-09-14 16:38:36 +0100

    add Footer Test

--- populate_data/github_data.rb
+++ populate_data/github_data.rb
@@ -11,21 +11,20 @@ require './data_sets/github_org_names.rb'
 db_config = YAML.load_file('config/database.yml')
 ActiveRecord::Base.establish_connection(db_config['development'])
 
-# complete redo of org names
+# fill in new org names
 def fill_org_name
   relevant_orgs =  GithubOrgNames::GITHUB_MAP.reject {|_,v| v == 'N/A'}
   relevant_orgs.each do |k,v|
-    if DigitalAsset.where(ticker: k)[0].github_profile
-      DigitalAsset.where(ticker: k)[0].github_profile.update_attribute(:org_name, v)
-    else
+    if DigitalAsset.where(ticker: k)[0].github_profile == nil
       DigitalAsset.where(ticker: k)[0].create_github_profile(org_name: v)
     end
+    puts DigitalAsset.where(ticker: k)[0].github_profile
   end
 end
 
-def build_github_profile(ticker, github_name)
-  asset = DigitalAsset.where(ticker: ticker).first
-  profile = asset.create_github_profile(org_name: github_name)
+def buid_github_profile(asset)
+  profile = asset.github_profile
+  require 'pry'; binding.pry
   profile.update_profile
   profile.create_repos
   profile.update_repos
@@ -34,4 +33,8 @@ end
 
 
 fill_org_name
+DigitalAsset.first(10).each do
+  |asset|
+  buid_github_profile(asset)
+end
 # build_github_profile('QTUM', 'qtumproject')
